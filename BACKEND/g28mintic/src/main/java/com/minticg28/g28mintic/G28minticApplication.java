ae8353f7bdf79162abcb9f98f963866dabaab775
rnlpz <carlos.erazo@o365.unab.co>
2022-11-25 13:32:59 -0500

    ensayo

--- BACKEND/g28mintic/src/main/java/com/minticg28/g28mintic/G28minticApplication.java
+++ BACKEND/g28mintic/src/main/java/com/minticg28/g28mintic/G28minticApplication.java
@@ -2,7 +2,7 @@ package com.minticg28.g28mintic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*ejemplo */
 @SpringBootApplication
 public class G28minticApplication {
 
